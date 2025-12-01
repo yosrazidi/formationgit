d705f3d32177cead3fec2e0fda845c203c45448e
derbe <derbely887@gmail.com>
2025-12-01 11:54:23 +0100

    v0

--- src/main/java/com/example/gitgithub/file1.java
+++ src/main/java/com/example/gitgithub/file1.java
@@ -2,24 +2,8 @@ package com.example.gitgithub;
 
 public class file1 {
     private String message = "Hello, GitHub!";
-    private String author = "Mohamed Yassine Derbel";
-    private String version = "1.0.0"; 
-
-    private String description = "Formation Git"; 
-
     public String getMessage() {
         return message;
     }
     
-    public void setMessage(String message) {
-        this.message = message;
-    }
-    
-    public String getAuthor() {
-        return author;
-    }
-    
-    public void setAuthor(String author) {
-        this.author = author;
-    }
 }
